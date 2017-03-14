a0313f02212a1cf997563a95533f7c50cf18a591
muchilafred <f.m.muchila@rgu.ac.uk>
2017-03-14 18:45:51 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,9 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1513133
- * Date: 07/03/2017
- * Time: 16:33
- */
+<?PHP
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
+
 foreach ($_SERVER as $key => $value) {
     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
         continue;
@@ -19,8 +14,16 @@ foreach ($_SERVER as $key => $value) {
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+
+define('DB_SERVER', $connectstr_dbhost);
+define('DB_USERNAME', $connectstr_dbname);
+define('DB_PASSWORD', $connectstr_dbusername);
+define('DB_DATABASE', $connectstr_dbpassword);
+
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
 if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Error: Una ble to  connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
